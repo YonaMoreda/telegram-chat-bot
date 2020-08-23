b8fc5b06390160f1ed9169721dbf904070baee0f
YonaMoreda <yokots3q@gmail.com>
2020-08-23 16:50:27 +0200

    small refactor

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import logging
 import atexit
 import configparser
@@ -8,31 +9,34 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 config = configparser.ConfigParser()
 config.read('configuration.ini')
 
-
 cb = cleverbotfree.cbfree.Cleverbot()
-cb.browser.get(cb.url)
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-bot_token = config['TOKEN']['BotToken']
-updater = Updater(token=bot_token, use_context=True)
-dispatcher = updater.dispatcher
+"""
+function for '/start' command
+"""
 
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="Hi 🙂,\n\n"
-                                  "I am RecVel, a simple blabbering bot.\n\n"
-                                  "I won't read your messages unless you @ me 😩\n - @RecVel_bot")
+                             text="Hi,\n\n"
+                                  "I am RecVel, a simple blabbering bot based on cleverbot.com\n\n"
+                                  "Use @RecVel_bot for me to read your message.\n")
 
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+"""
+function for handling messages that are not commands
+"""
 
 
 def chat(update, context):
-    cb.get_form()
+    try:
+        cb.get_form()
+    except Exception as e:
+        sys.exit(e)
+
     received_msg = update.message.text
 
     if '@RecVel_bot' in received_msg:
@@ -43,15 +47,43 @@ def chat(update, context):
                                  reply_to_message_id=update.message.message_id)
 
 
+"""
+cleaning up function when exiting the bot
+"""
+
+
 def exit_handler():
     cb.browser.close()
     print("exited bot")
 
 
-atexit.register(exit_handler)
+"""
+main starter function, register the function handlers and start the polling the bot
+"""
 
-echo_handler = MessageHandler(Filters.text & (~Filters.command), chat)
-dispatcher.add_handler(echo_handler)
 
-updater.start_polling()
-print("Started bot")
+def main():
+    bot_token = config['TOKEN']['BotToken']
+    atexit.register(exit_handler)
+
+    updater = Updater(token=bot_token, use_context=True)
+    dispatcher = updater.dispatcher
+
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
+
+    try:
+        cb.browser.get(cb.url)  # for using clever bot
+    except Exception as e:
+        cb.browser.close()
+        sys.exit(e)
+
+    echo_handler = MessageHandler(Filters.text & (~Filters.command), chat)
+    dispatcher.add_handler(echo_handler)
+
+    updater.start_polling()
+    print("Started bot")
+
+
+if __name__ == '__main__':
+    main()
